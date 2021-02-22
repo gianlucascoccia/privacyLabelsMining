2ed4c2446f71feb6d7a863e6d5343a71f706c4ee
Gian Luca Scoccia <loscoccia@gmail.com>
2021-02-22 14:28:54 +0100

    first commit

---
+++ notebook/parse_html.py
@@ -0,0 +1,68 @@
+# %% Imports
+
+import csv
+import glob
+import unicodedata
+import os.path as path
+from bs4 import BeautifulSoup
+
+# %% Load html files
+
+filesFolder = "../data/raw/rawHTML"
+html_files = glob.glob(path.join(filesFolder, "*.htm"))
+
+# %% Set up output file
+
+OUT_FILE = "../data/processed/top_apps_list.csv"
+
+out_file = open(OUT_FILE, 'w')
+writer = csv.DictWriter(out_file, fieldnames=['id', 'top_chart_position','category','name','publisher','store_id'], delimiter=";", quoting=csv.QUOTE_MINIMAL) 
+writer.writeheader()
+
+# %% parse files
+
+# initialize unique app id
+app_id = 1
+
+for f in html_files:
+    # read file
+    soup = BeautifulSoup(open(f).read())
+
+    # extract file name (AKA category name)
+    category = path.basename(f).split(".")[0]
+    
+    # get html list
+    app_list = soup.find('top-apps-col')
+    apps = app_list.findAll('app-list-item')
+
+    # parse list
+    parsed_app = {}
+    for app in apps:
+
+        # get app chart position
+        app_position = int(app.find("div", {"class": "top-position-number"}).text)
+
+        # get app name
+        name = unicodedata.normalize("NFKD",str(app.find("a", {"class": "app-name"}).text))
+
+        # get app publisher
+        publisher = unicodedata.normalize("NFKD",str(app.find("a", {"app-publisher-name"}).text))
+
+        # get app store id
+        store_id = app.find("a", {"class": "app-name"})['href'].split('/')[3]
+        
+        # update results
+        parsed_app.update({
+            'id': app_id,
+            'top_chart_position': app_position,
+            'category': category,
+            'name': name,
+            'publisher': publisher,
+            'store_id': store_id
+        })
+
+        app_id = app_id + 1 
+
+        writer.writerow(parsed_app)
+
+# %%
---
+++ notebook/scraper/scrape_app.js
@@ -0,0 +1,3 @@
+var store = require('app-store-scraper');
+
+store.app({id: 1492703474}).then(console.log).catch(console.log);
